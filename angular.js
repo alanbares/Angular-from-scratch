639db04c4dc8753af7e48f4fa6a5539980f5ea4b
alanbares <neloice@gmail.com>
2016-04-05 15:53:24 -0700

    Add watch function and test.

--- angular.js
+++ angular.js
@@ -6,6 +6,15 @@
 
   var Scope = function() {
 
+    this.$$watchers = [];
+
+    this.$watch = function(watcherFn, listenerFn) {
+      this.$$watchers.push({
+        watcherFn: watcherFn,
+        listenerFn: listenerFn
+      });
+    };
+
   };
 
   var $scope = new Scope();
@@ -14,3 +23,9 @@
 // tests & logs
 
 $scope.hello = 'Hello World';
+
+$scope.$watch(function() {
+  return $scope.hello;
+}, function(newValue, oldValue) {
+  console.log('hello changed from', oldValue, 'to', newValue);
+});
